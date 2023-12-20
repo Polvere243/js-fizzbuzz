5c976935062001ba39471a46389875a9d7decb4d
Polvere243 <danielercoli@gmail.com>
2023-12-20 20:43:46 +0100

    logged numbers from 1 to 100 in console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("JS OK");
+// creare un elemento;
+
+const square = document.createElement("div");
+
+// stampare in pagina i numeri da 1 a 100;
+console.log(square);
+
+for(let i = 1; i <= 100; i++){
+    console.log(i);
+    
+}
