14c62754c9a46aa30ce3975a5a0485d85ba3430a
Polvere243 <danielercoli@gmail.com>
2023-12-20 21:32:03 +0100

    logged again numbers from 1 to 100 in console with a different approach

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // creare un elemento;
 
-const square = document.createElement("div");
+// const square = document.createElement("div");
 
 // stampare in pagina i numeri da 1 a 100;
-console.log(square);
 
-for(let i = 1; i <= 100; i++){
-    console.log(i);
+
+for(let i = 0; i < 100; i++){
+    console.log(i+1);
     
 }
