21faeb5a05528d84374074352e5019db355a5b17
Polvere243 <danielercoli@gmail.com>
2023-12-25 01:13:27 +0100

    Done the basic exercise

--- js/script.js
+++ js/script.js
@@ -5,7 +5,20 @@
 // stampare in pagina i numeri da 1 a 100;
 
 
-for(let i = 0; i < 100; i++){
-    console.log(i+1);
+for(let i = 1; i <= 100; i++){
+
+    let content = i;
+    if(i % 15 === 0){
+        console.log("FizzBuzz");
+    } else if(i % 3 === 0){
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else{
+        console.log(content);
+
+    }
+    
+    
     
 }
