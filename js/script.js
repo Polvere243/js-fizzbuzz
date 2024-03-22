2ecd789cbad43fc783a6ea048739974c064c65ae
Polvere243 <danielercoli@gmail.com>
2024-03-22 20:36:09 +0100

    fixed capital letters

--- js/script.js
+++ js/script.js
@@ -6,20 +6,20 @@ const list = document.getElementById("squares-list");
 // stampare in pagina i numeri da 1 a 100;
 
 let items = "";
-for(let i = 1; i <= 100; i++){
+for (let i = 1; i <= 100; i++) {
 
     let content = i;
-    if(i % 15 === 0){
-        content = "FizzBuzz";
-    } else if(i % 3 === 0){
-        content = "Fizz";
+    if (i % 15 === 0) {
+        content = "fizzbuzz";
+    } else if (i % 3 === 0) {
+        content = "fizz";
     } else if (i % 5 === 0) {
-        content = "Buzz"
-    } else{
+        content = "buzz"
+    } else {
         content = `${i}`;
     }
 
-   items += `<li class="${content}">${content}</li>`;
+    items += `<li class="${content}">${content}</li>`;
 }
 
 list.innerHTML = items;
